b77a4333cad7d408bcd982ca5fd425b6ae6097a4
Mwita Chacha <jonahhchacha@gmail.com>
2021-10-15 17:58:37 +0300

    Putting environments into gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  gitAccessToken: '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'
 };
 
 /*
